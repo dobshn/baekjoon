9620e81ff5e4862eb5f1f8ca8168a7a62cbafb60
dobshn <dobshn@gmail.com>
2024-07-03 00:01:50 +0900

    부분합 구하기

--- src/p2042.java
+++ src/p2042.java
@@ -9,30 +9,22 @@ public class p2042 {
         int M = scanner.nextInt();
         int K = scanner.nextInt();
         scanner.nextLine();
-        System.out.println("N: " + N);
-        System.out.println("M: " + M);
-        System.out.println("K: " + K);
 
         // N개의 수를 array에 입력받기
         int[] numbers = new int[N];
         for (int i = 0; i < N; i++) {
             numbers[i] = Integer.parseInt(scanner.nextLine());
         }
-        System.out.println(Arrays.toString(numbers));
 
         for (int i = 0; i < M + K; i++) {
-            if (scanner.nextInt() == 1) {
+            int command = scanner.nextInt();
+            if (command == 1) {
                 int index = scanner.nextInt() - 1;
                 int number = scanner.nextInt();
                 numbers[index] = number;
-                scanner.nextLine();
-                System.out.println(index + "번째 인덱스에 " + number + "값으로 변경");
-                System.out.println(Arrays.toString(numbers));
-            } else if (scanner.nextInt() == 2){
+            } else if (command == 2){
                 int startIndex = scanner.nextInt() - 1;
                 int endIndex = scanner.nextInt() - 1;
-                scanner.nextLine();
-                System.out.println("시작 값: " + startIndex + "끝 값: " + endIndex);
                 int sum = 0;
                 for (int j = startIndex; j <= endIndex; j++) {
                     sum += numbers[j];
